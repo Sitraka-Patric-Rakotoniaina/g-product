b5a52f85bde2f20cf5165c4e4cca9773315d1a68
Sitraka <sitrakiniainaptc@gmail.com>
2023-09-09 17:51:14 +0300

    login page control

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,11 +12,20 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        if (!is_null($this->getUser())) {
+            return $this->redirectToRoute('app_product_index');
+        }
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('login/index.html.twig', [
+        return $this->render('login/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
+    {
+        throw new \Exception('logout');
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,8 +12,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/product')]
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'app_product_index', methods: ['GET'])]
@@ -30,7 +30,7 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_product_new', methods: ['GET', 'POST'])]
+    #[Route('/product/new', name: 'app_product_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $product = new Product();
@@ -50,7 +50,7 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_product_edit', methods: ['GET', 'POST'])]
+    #[Route('/product/{id}/edit', name: 'app_product_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Product $product, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ProductType::class, $product);
@@ -68,7 +68,7 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/delete/{id}', name: 'app_product_delete')]
+    #[Route('/product/delete/{id}', name: 'app_product_delete')]
     public function delete(Request $request, Product $product, EntityManagerInterface $entityManager): Response
     {
         $entityManager->remove($product);
